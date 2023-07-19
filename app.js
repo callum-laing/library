bb6f8148a10f39e0472cc77d0af39c02423839ac
Callum Laing <callum.laing91@hotmail.co.uk>
2023-07-19 21:39:24 +0100

    added functions

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-let myLibrary = [];
+let myLibrary = [
+  {
+    title: "A Game of Thrones",
+    author: "George R. R. Martin",
+    pages: 694,
+    read: false,
+  },
+];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+const addBookToLibrary = () => {
+  let title = titleInput.value;
+  let author = authorInput.value;
+  let pages = pagesInput.value;
+  let read = getReadValue();
+  let newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+};
